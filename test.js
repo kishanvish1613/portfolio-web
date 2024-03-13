4ee754dc9eefcb00d7533abfe93daa3dbfe6db93
kishanvish1613 <100762979+kishanvish1613@users.noreply.github.com>
2024-03-13 17:37:03 +0530

    final commit

---
+++ test.js
@@ -0,0 +1,6 @@
+// Example 1: Accessing global object properties
+console.log(global.process);
+
+// Example 2: Defining a global variable
+global.myGlobalVariable = 'Hello, world!';
+console.log(myGlobalVariable);
