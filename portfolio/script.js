bc4e8689875ac6b6668bc2d0ae77ff69ab06045b
kishanvish1613 <100762979+kishanvish1613@users.noreply.github.com>
2024-03-19 16:46:11 +0530

    added link and some changes

--- portfolio/script.js
+++ portfolio/script.js
@@ -174,3 +174,52 @@ contectAnimation();
 // }
 
 // more();
+
+// Function to apply styles
+function applyStyles(elements, styles) {
+  elements.forEach((element) => {
+    Object.assign(element.style, styles);
+  });
+}
+
+// Function to handle mouseover event
+function handleMouseOver(event) {
+  const projectBox = event.currentTarget;
+  const elements = projectBox.querySelectorAll(
+    ".top-text, .bottom-text, .right-box-top-text, .github-link-project, .overflow-link"
+  );
+  applyStyles(elements, {
+    color: "rgb(63, 69, 69)",
+    transition: "color 0.5s ease, right 0.5s ease",
+    cursor: "pointer",
+  });
+  const overflowLink = projectBox.querySelector(".overflow-link");
+  overflowLink.style.right = "0%";
+}
+
+// Function to handle mouseout event
+function handleMouseOut(event) {
+  const projectBox = event.currentTarget;
+  const elements = projectBox.querySelectorAll(
+    ".top-text, .bottom-text, .right-box-top-text, .github-link-project, .overflow-link"
+  );
+  applyStyles(elements, {
+    color: "",
+    transition: "color 0.5s ease, right 0.5s ease",
+  });
+  const overflowLink = projectBox.querySelector(".overflow-link");
+  overflowLink.style.right = "";
+}
+
+// Select project boxes
+const projectBox1 = document.querySelector(".project-box1");
+const projectBox2 = document.querySelector(".project-box2");
+const projectBox3 = document.querySelector(".project-box3");
+
+// Add event listeners
+projectBox1.addEventListener("mouseover", handleMouseOver);
+projectBox1.addEventListener("mouseout", handleMouseOut);
+projectBox2.addEventListener("mouseover", handleMouseOver);
+projectBox2.addEventListener("mouseout", handleMouseOut);
+// projectBox3.addEventListener("mouseover", handleMouseOver);
+// projectBox3.addEventListener("mouseout", handleMouseOut);
